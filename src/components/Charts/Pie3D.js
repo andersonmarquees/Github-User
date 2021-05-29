cfb617781ab4946271c8fd79fd7739781b71d8bc
Anderson Marques <anderson_fisico@yahoo.com.br>
2021-05-29 18:04:16 -0300

    charts :construction:

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Chart from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
-const Pie3D = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "pie3d", // The chart type
+    width: "400", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Languages",
+        theme: "fusion",
+        decimals: 0,
+        pieRadius: "50%",
+      },
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Pie3D;
+export default ChartComponent;
